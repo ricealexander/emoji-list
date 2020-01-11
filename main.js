df6aef91ca5eea90ff03c6a95035c21078844174
ricealexander <webdev@ricealexander>
2020-01-10 21:34:24 -0600

    :sparkles: build sort function to sort emojis

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-module.exports = emojiJSON => `Build process works! ${JSON.stringify(emojiJSON)}`
+// const by = require('./src/by')
+
+const sortByUnicode = ({ unicode: emojiA }, { unicode: emojiB }) => {
+  // force false values to be at the bottom of the array
+  if (emojiB === false) return -1
+  if (emojiA === false) return 1
+
+  // sort alphabetically
+  if (emojiA > emojiB) return 1
+  if (emojiA < emojiB) return -1
+  return 0
+}
+
+module.exports = function (emojiJSON) {
+  const sortedEmoji = emojiJSON.sort(sortByUnicode)
+  return JSON.stringify(sortedEmoji, null, 2)
+}
