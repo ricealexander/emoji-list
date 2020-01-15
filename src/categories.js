4c74ecea9a3b6cfeaacd957ff42235c58531b07f
ricealexander <webdev@ricealexander>
2020-01-14 20:41:03 -0600

    :sparkles: Extract categories list to a module

---
+++ src/categories.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-multi-spaces */
+
+const categories = [
+  { name: 'People',    displayName: 'Smileys and People'  },
+  { name: 'Nature',    displayName: 'Animals and Nature'  },
+  { name: 'Food',      displayName: 'Food and Drink'      },
+  { name: 'Activity',  displayName: 'Activity'            },
+  { name: 'Travel',    displayName: 'Travel and Places'   },
+  { name: 'Objects',   displayName: 'Objects'             },
+  { name: 'Symbols',   displayName: 'Symbols'             },
+  { name: 'Countries', displayName: 'Flags and Countries' },
+]
+
+const categoryOrder = categories.map(({name}) => name)
+
+// { People: 'Smileys and People', Nature: 'Animals and Nature', ... }
+const categoriesObject = Object.fromEntries(categories.map(Object.values))
+
+module.exports = {
+  categories,
+  categoriesObject,
+  categoryOrder,
+}
