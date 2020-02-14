e4a5b2969b4ca626bef94a167d8266c765a81d31
ricealexander <webdev@ricealexander>
2020-02-13 22:23:26 -0600

    :recycle: Refactor parseKeyTrail
    parseKeyTrail is a helper used for optional chaining: parseKeyTrail(user, 'adresses.primary.zip') returns the value of user.addresses.primary.zip or undefined

--- src/helpers/parseKeyTrail.js
+++ src/helpers/parseKeyTrail.js
@@ -1,17 +1,14 @@
+const followTrail = (level, key) => (level === undefined) ? level : level[key]
+
 function parseKeyTrail (object, string) {
-  if (!object || typeof object !== 'object' ||
-      !string || typeof string !== 'string'
+  if (
+    !object || typeof object !== 'object' ||
+    !string || typeof string !== 'string'
   ) return undefined
 
-  function followTrail (objectLevel, key) {
-    if (objectLevel === undefined || objectLevel[key] === undefined) {
-      return undefined
-    }
-    return objectLevel[key]
-  }
-
-  const trail = string.split('.')
-  return trail.reduce((a, b) => followTrail(a, b), object)
+  return string
+    .split('.')
+    .reduce(followTrail, object)
 }
 
 module.exports = parseKeyTrail
